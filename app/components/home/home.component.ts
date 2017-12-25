4502a43e74e607154367ead22ab87985444530a8
Luke Chen Shui <chenshuiluke@gmail.com>
2017-12-25 02:27:39 -0500

    Added input debounce

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -3,6 +3,9 @@ import { SearchBar } from "ui/search-bar";
 import { environment } from "../../environment/environment";
 import { SearchService } from "../../services";
 import { Word } from '../../models';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 @Component({
     selector:'home',
     styleUrls:['components/home/home.component.css'],
@@ -10,26 +13,37 @@ import { Word } from '../../models';
 })
 export class HomeComponent{
     words:Word[];
+    userSearched: Subject<string> = new Subject<string>();
     constructor(private search:SearchService){
+        this.userSearched
+        .debounceTime(500) // wait 500ms after the last event before emitting last event
+        .distinctUntilChanged() // only emit if value is different from previous value
+        .subscribe((word:String) => {
+            this.searchForTerm(word);
+        });
     }
-    searchForTerm(event){
-        if(event.value){
-            console.log("Searching");
-            console.log(event.value);
-            this.search.searchForWord(event.value)
-                .then((words:Word[]) => {
-                    this.words = words;
-                    this.words.map((item:Word) => {
-                        console.log(item.word);
-                        return item;
-                    })
-                })
-                .catch((err) => {
-                    console.log(err);   
-                });
 
-            this.clearBox();
+    registerKeyPress(event){
+        if(event.value){
+            this.userSearched.next(event.value)
         }
+    }
+    searchForTerm(word:String){
+        console.log("Searching");
+        console.log(word);
+        this.search.searchForWord(word)
+            .then((words:Word[]) => {
+                this.words = words;
+                this.words.map((item:Word) => {
+                    console.log(item.word);
+                    return item;
+                })
+            })
+            .catch((err) => {
+                console.log(err);   
+            });
+
+        this.clearBox();
     }
 
     clearBox(){
